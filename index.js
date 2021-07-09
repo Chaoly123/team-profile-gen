a4c10a5cfab76eb5522a7cf420f1cc6cf26f9956
Chao Ly <lychaos123@gmail.com>
2021-07-09 16:11:50 -0500

    add the homework assignment and replace the readme with the homework readme.

---
+++ index.js
@@ -0,0 +1,23 @@
+// To validate email in inquirer
+
+// return inquirer
+//   .prompt(
+//     {
+//       message: "Please enter your email",
+//       name: "email",
+//       type: "input",
+//       default: () => { },
+//       validate: function (email) {
+
+//         valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+//         if (valid) {
+//           console.log("Great job");
+//           return true;
+//         } else {
+//           console.log(".  Please enter a valid email")
+//           return false;
+//         }
+//       }
+//     }
+//   );
